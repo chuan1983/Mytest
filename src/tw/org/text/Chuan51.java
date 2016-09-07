c94662fcadb3a6dadb761bc3c3cc99dacbd04664
chuan1983 <chuan1983126@gmail.com>
2016-09-07 13:03:22 +0800

    9/7 am13

--- src/tw/org/text/Chuan51.java
+++ src/tw/org/text/Chuan51.java
@@ -8,7 +8,7 @@ import java.net.UnknownHostException;
 
 public class Chuan51 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {         //設定傳封包
 		String data = "Hello word";
 		byte[] buf = data.getBytes();
 		try {
@@ -16,7 +16,8 @@ public class Chuan51 {
 					new DatagramSocket();
 			DatagramPacket packet = 
 					new DatagramPacket(buf, buf.length, 
-							InetAddress.getByName("10.1.6.65"), 8888);
+							InetAddress.getByName("10.1.6.65"), 8888);        //傳封包需要對方的ip和port號
+			
 			socket.send(packet);
 			socket.close();
 			System.out.println("Send Ok");
